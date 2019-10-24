70f43e5f5d7a6e52dcf13793e0bf3b1bf55d870f
Mat Trudel <mat@geeky.net>
2019-10-24 13:31:23 -0400

    Update parser test to use idiomatic Plug.Test constructs

--- test/hap/tlv_parser_test.exs
+++ test/hap/tlv_parser_test.exs
@@ -1,5 +1,6 @@
 defmodule HAP.TLVParserTest do
   use ExUnit.Case
+  use Plug.Test
 
   test "parses multi-segment entries properly" do
     data =
@@ -7,8 +8,15 @@ defmodule HAP.TLVParserTest do
         :binary.copy(<<0xAA>>, 255) <>
         <<1, 255>> <> :binary.copy(<<0xAB>>, 255) <> <<1, 10>> <> :binary.copy(<<0xBA>>, 10) <> <<2, 1, 3>>
 
-    expected = :binary.copy(<<0xAA>>, 255) <> :binary.copy(<<0xAB>>, 255) <> :binary.copy(<<0xBA>>, 10)
+    expected = %{
+      1 => :binary.copy(<<0xAA>>, 255) <> :binary.copy(<<0xAB>>, 255) <> :binary.copy(<<0xBA>>, 10),
+      2 => <<3>>
+    }
 
-    assert HAP.TLVParser.parse_tlv(data) == %{1 => expected, 2 => <<3>>}
+    {:ok, result, _conn} =
+      conn("METHOD", "path", data)
+      |> HAP.TLVParser.parse("application", "pairing+tlv8", {}, {})
+
+    assert result == expected
   end
 end
