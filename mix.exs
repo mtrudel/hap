97cf54858c554a767f1c7be85ff27ce7f992fabd
Mat Trudel <mat@geeky.net>
2020-12-13 20:42:02 -0500

    Version bump to 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HAP.MixProject do
   def project do
     [
       app: :hap,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
