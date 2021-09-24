4f413f392b82c72ad21fd5aa0eec6f6dcf44ef9e
Peter Madsen-mygdal <petermm@gmail.com>
2021-09-24 16:32:46 +0200

    test:  config :mdns_lite, skip_udp: true

--- config/config.exs
+++ config/config.exs
@@ -1,16 +1,5 @@
 import Config
 
 if Mix.env() == :test do
-  config :vintage_net,
-    udhcpc_handler: VintageNetTest.CapturingUdhcpcHandler,
-    udhcpd_handler: VintageNetTest.CapturingUdhcpdHandler,
-    interface_renamer: VintageNetTest.CapturingInterfaceRenamer,
-    resolvconf: "/dev/null",
-    path: "#{File.cwd!()}/test/fixtures/root/bin",
-    persistence_dir: "./test_tmp/persistence",
-    power_managers: [
-      {VintageNetTest.TestPowerManager, [ifname: "test0", watchdog_timeout: 50]},
-      {VintageNetTest.BadPowerManager, [ifname: "bad_power0"]},
-      {NonExistentPowerManager, [ifname: "test_does_not_exist_case"]}
-    ]
+  config :mdns_lite, skip_udp: true
 end
