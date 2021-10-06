7a90972223d253803fba3aa5b2e5c9b02103bfe9
Peter Madsen-mygdal <petermm@gmail.com>
2021-10-07 00:33:15 +0200

    bump dialyzer_ignore

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,5 @@
 # .dialyzer_ignore.exs
 [
-  {"lib/hap/kino_display.ex:17:unknown_function Function Kino.Markdown.new/1 does not exist."},
-  {"lib/hap/kino_display.ex:27:unknown_function Function Kino.render/1 does not exist."}
+  {"lib/hap/kino_display.ex:0:unknown_function Function Kino.Markdown.new/1 does not exist."},
+  {"lib/hap/kino_display.ex:0:unknown_function Function Kino.render/1 does not exist."}
 ]
