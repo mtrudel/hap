65a845cf1c95aa466a92000b356a4af0f74e0cb2
Kamil Wąż <kamilwaz@icloud.com>
2024-05-08 10:48:08 +0200

    Fix ChaCha20 encryption for newer OpenSSL versions

--- lib/hap/crypto/cha_cha_20.ex
+++ lib/hap/crypto/cha_cha_20.ex
@@ -11,7 +11,7 @@ defmodule HAP.Crypto.ChaCha20 do
   @doc """
   Takes a binary containing encrypted data followed by a 16 byte tag, verifies the tag
   and decrypts the resultant data using the given key and nonce. Can take optional AAD
-  data which is authenticated under the auth_tag but not encrypted. 
+  data which is authenticated under the auth_tag but not encrypted.
 
   Returns `{:ok, plaintext}` or `{:error, message}`
   """
@@ -21,7 +21,7 @@ defmodule HAP.Crypto.ChaCha20 do
     encrypted_data_length = byte_size(encrypted_data) - 16
     <<encrypted_data::binary-size(encrypted_data_length), auth_tag::binary-16>> = encrypted_data
 
-    case :crypto.crypto_one_time_aead(:chacha20_poly1305, key, nonce, encrypted_data, aad, auth_tag, false) do
+    case :crypto.crypto_one_time_aead(:chacha20_poly1305, key, pad(nonce), encrypted_data, aad, auth_tag, false) do
       :error -> {:error, "Message decryption error"}
       result -> {:ok, result}
     end
@@ -30,13 +30,24 @@ defmodule HAP.Crypto.ChaCha20 do
   @doc """
   Takes a plaintext binary and encrypts & tags it using the given key & nonce. Optionally takes
   AAD data which is authenticated under the auth tag but not included in the returned binary (it is
-  up to the caller to convey the AAD to their counterparty). 
+  up to the caller to convey the AAD to their counterparty).
 
   Returns `{:ok, encrypted_data <> auth_tag}`
   """
   @spec encrypt_and_tag(plaintext(), key(), nonce(), aad()) :: {:ok, ciphertext_with_authdata()}
   def encrypt_and_tag(plaintext, key, nonce, aad \\ <<>>) do
-    {encrypted_data, auth_tag} = :crypto.crypto_one_time_aead(:chacha20_poly1305, key, nonce, plaintext, aad, true)
+    {encrypted_data, auth_tag} =
+      :crypto.crypto_one_time_aead(:chacha20_poly1305, key, pad(nonce), plaintext, aad, true)
+
     {:ok, encrypted_data <> auth_tag}
+  end
+
+  @nonce_size 12
+
+  defp pad(nonce) when byte_size(nonce) >= @nonce_size, do: nonce
+
+  defp pad(nonce) do
+    bits = (@nonce_size - byte_size(nonce)) * 8
+    <<0::size(bits), nonce::binary>>
   end
 end
